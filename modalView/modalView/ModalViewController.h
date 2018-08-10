ca32685066d2182148c95b979042712c1029a2d9
陈乐杰 <1946363509@qq.com>
2018-08-10 11:00:08 +0800

    push

--- modalView/modalView/ModalViewController.h
+++ modalView/modalView/ModalViewController.h
@@ -13,6 +13,7 @@ typedef void(^textBlock)(NSString *text);
 //用来调用的block，并且可以被外部传值
 @property (strong,nonatomic) textBlock textB;
 -(void)doBlock;
-//需要初始化的额外内容，可以用来传递通用的block，一般传nil
+//需要初始化的额外内容，可以用来传递通用的block，一般传nil,如过是继承此类会出问题，会加载到父类，还需要解决,目前直接alloc，init
+//但是要设置VC.modalPresentationStyle = UIModalPresentationOverFullScreen;
 +(instancetype)initWithBlock:(modalBlock)block;
 @end
